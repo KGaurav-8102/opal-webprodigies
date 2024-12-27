b04d8026a36c21ab5a3b90d24ffeaf8ff67ceda6
Gaurav <gk5480325@gmail.com>
2024-12-27 18:48:56 +0530

    we have created navbar and sign in page started

---
+++ src/app/(website)/_components/navbar.tsx
@@ -0,0 +1,33 @@
+
+import { Button } from "@/components/ui/button";
+import { Menu, User } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type Props = {}
+
+const LandingPageNavbar = (props: Props) => {
+    return (
+        <div className="flex w-full justify-between items-center">
+            <div className="text-3xl font-semibold flex items-center gap-2">
+                <Menu className="w-6 h-6" />
+                <Image src="./opal-logo.svg" alt="logo" width={40} height={40} />
+                Opal
+            </div>
+            <div className="hidden gap-x-10 items-center lg:flex">
+                <Link href="/" className="bg-[#7230DD] py-2 px-5 font-semibold text-lg rounded-full hover:bg-[#7230DD]/80">Home</Link>
+                <Link href="/">Pricing</Link>
+                <Link href="/">Contact</Link>
+            </div>
+            <Link href="/auth/sign-in">
+                <Button className="text-base flex gap-x-2">
+                    <User fill="#000" />
+                    Login</Button>
+            </Link> 
+        </div>
+    )
+}
+
+export default LandingPageNavbar;
+
